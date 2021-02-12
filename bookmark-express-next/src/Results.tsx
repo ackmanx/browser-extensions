93b4772a63714f54b4a925e2e60635be442c9891
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-12 11:54:47 -0600

    yay

--- bookmark-express-next/src/Results.tsx
+++ bookmark-express-next/src/Results.tsx
@@ -11,35 +11,25 @@ import {
 import FolderIcon from '@material-ui/icons/Folder'
 import DeleteIcon from '@material-ui/icons/Delete'
 
-function generate(element: any) {
-    return [0, 1, 2].map((value) =>
-        React.cloneElement(element, {
-            key: value,
-        })
-    )
-}
-
 export function Results() {
     return (
         <List>
-            {generate(
-                <ListItem>
-                    <ListItemAvatar>
-                        <Avatar>
-                            <FolderIcon />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary='What is the Execution Context & Stack in JavaScript? by David Shariff'
-                        secondary='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND'
-                    />
-                    <ListItemSecondaryAction>
-                        <IconButton edge='end' aria-label='delete'>
-                            <DeleteIcon />
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>
-            )}
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar>
+                        <FolderIcon />
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                    primary='What is the Execution Context & Stack in JavaScript? by David Shariff'
+                    secondary='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND'
+                />
+                <ListItemSecondaryAction>
+                    <IconButton edge='end' aria-label='delete'>
+                        <DeleteIcon />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
         </List>
     )
 }
