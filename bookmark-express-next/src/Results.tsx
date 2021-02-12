26aa0684036183522a1535195223489a59d6cac0
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-12 11:07:39 -0600

    added material ui list example for results

--- bookmark-express-next/src/Results.tsx
+++ bookmark-express-next/src/Results.tsx
@@ -1,32 +1,45 @@
 import React from 'react'
+import {
+    Avatar,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemSecondaryAction,
+    ListItemText,
+} from '@material-ui/core'
+import FolderIcon from '@material-ui/icons/Folder'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+function generate(element: any) {
+    return [0, 1, 2].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        })
+    )
+}
 
 export function Results() {
     return (
-        <ul>
-            <li>
-                <div>What is the Execution Context & Stack in JavaScript? by David Shariff</div>
-                <div>
-                    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND
-                </div>
-            </li>
-            <li>
-                <div>What is the Execution Context & Stack in JavaScript? by David Shariff</div>
-                <div>
-                    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND
-                </div>
-            </li>
-            <li>
-                <div>What is the Execution Context & Stack in JavaScript? by David Shariff</div>
-                <div>
-                    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND
-                </div>
-            </li>
-            <li>
-                <div>What is the Execution Context & Stack in JavaScript? by David Shariff</div>
-                <div>
-                    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND
-                </div>
-            </li>
-        </ul>
+        <List>
+            {generate(
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <FolderIcon />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary='What is the Execution Context & Stack in JavaScript? by David Shariff'
+                        secondary='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND'
+                    />
+                    <ListItemSecondaryAction>
+                        <IconButton edge='end' aria-label='delete'>
+                            <DeleteIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            )}
+        </List>
     )
 }
