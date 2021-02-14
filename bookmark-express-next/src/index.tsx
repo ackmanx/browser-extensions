5b0002ac309871c1651274dc8c56568d6bb20f26
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-14 16:50:52 -0600

    added stale cache check

--- bookmark-express-next/src/index.tsx
+++ bookmark-express-next/src/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@fontsource/roboto'
+import { browser } from 'webextension-polyfill-ts'
+import md5 from 'md5'
 import './index.css'
 import { App } from './components/App'
+
+async function isCacheStale() {
+    const { children: bookmarks } = (await browser.bookmarks.getTree())[0]
+
+    const freshHash = md5(JSON.stringify(bookmarks))
+    const savedHash = localStorage.getItem('hash')
+
+    return freshHash !== savedHash
+}
+
+isCacheStale().then((result) => {
+    window.isCacheStale = result
+})
 
 ReactDOM.render(
     <React.StrictMode>
--- bookmark-express-next/src/react-app-env.d.ts
+++ bookmark-express-next/src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+export declare global {
+    interface Window {
+        isCacheStale: boolean
+    }
+}
