4b69cd6d5b42a4adc8822cfa90f5c88f844a7192
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-16 17:42:04 -0600

    matched search underline color to results highlight color

--- bookmark-express-next/src/components/SearchBar.tsx
+++ bookmark-express-next/src/components/SearchBar.tsx
@@ -1,24 +1,42 @@
 import React, { ChangeEvent, useContext } from 'react'
-import { TextField } from '@material-ui/core'
+import { makeStyles, TextField } from '@material-ui/core'
 import { browser } from 'webextension-polyfill-ts'
 import AppContext from '../context/AppContext'
-import { isFolder } from '../utils/misc-utils'
+import { isFolder } from '../utils/misc'
+
+const useStyles = makeStyles({
+    search: {
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#3576CB',
+        },
+    },
+})
 
 export function SearchBar() {
-    const resultsContext = useContext(AppContext)
+    const classes = useStyles()
+    const context = useContext(AppContext)
 
     const handleSearch = async (event: ChangeEvent<HTMLInputElement>) => {
         const query = event.target.value
 
         if (query.length <= 1) {
-            resultsContext.updateResults([])
+            context.updateResults([])
             return
         }
 
         const bookmarks = (await browser.bookmarks.search(query)).filter((bookmark) => !isFolder(bookmark))
 
-        resultsContext.updateResults(bookmarks)
+        context.updateResults(bookmarks)
     }
 
-    return <TextField fullWidth autoFocus placeholder='start typing' variant='standard' onChange={handleSearch} />
+    return (
+        <TextField
+            className={classes.search}
+            fullWidth
+            autoFocus
+            placeholder='start typing'
+            variant='standard'
+            onChange={handleSearch}
+        />
+    )
 }
