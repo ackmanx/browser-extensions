6e0cb527e56a21e6601b08ae31e153d6c1de1bf1
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-16 10:50:26 -0600

    whoops, I knew I'd do this

--- bookmark-express-next/src/utils/misc-utils.ts
+++ bookmark-express-next/src/utils/misc-utils.ts
@@ -2,5 +2,5 @@ import { Bookmarks } from 'webextension-polyfill-ts'
 
 export function isFolder(bookmark: Bookmarks.BookmarkTreeNode) {
     //Folders don't have URLs, and bookmarks do
-    return !!bookmark.url
+    return !bookmark.url
 }
