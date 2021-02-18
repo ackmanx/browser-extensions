965a6f62d33a7037e464a7b97ef4f5d2ccf93e12
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-18 12:06:54 -0600

    changed border color of input when focused to match the highlight color

--- bookmark-express-next/src/components/SearchBar.tsx
+++ bookmark-express-next/src/components/SearchBar.tsx
@@ -1,12 +1,21 @@
 import React, { ChangeEvent, useContext } from 'react'
-import { Grid, IconButton, OutlinedInput } from '@material-ui/core'
+import { Grid, IconButton, makeStyles, OutlinedInput } from '@material-ui/core'
 import SettingsIcon from '@material-ui/icons/Settings'
 import { browser } from 'webextension-polyfill-ts'
 import AppContext from '../context/AppContext'
 import { isFolder } from '../utils/misc'
 import FiberNewIcon from '@material-ui/icons/FiberNew'
 
+const useStyles = makeStyles({
+    search: {
+        '&.Mui-focused fieldset': {
+            borderColor: '#3576CB !important',
+        },
+    },
+})
+
 export function SearchBar() {
+    const classes = useStyles()
     const context = useContext(AppContext)
 
     const handleSearch = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -33,7 +42,13 @@ export function SearchBar() {
     return (
         <Grid container spacing={1}>
             <Grid item xs={10}>
-                <OutlinedInput fullWidth autoFocus placeholder='start typing' onChange={handleSearch} />
+                <OutlinedInput
+                    fullWidth
+                    autoFocus
+                    placeholder='start typing'
+                    onChange={handleSearch}
+                    className={classes.search}
+                />
             </Grid>
             <Grid item xs={2} container justify='center' alignItems='center'>
                 <IconButton onClick={handleShowRecentlyAddedBookmarks}>
