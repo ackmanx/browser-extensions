e33faa63e2bf56d654829f46a4c40906217a04d0
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-18 09:43:28 -0600

    breaking url on character so urls don't create horizontal scroll

--- bookmark-express-next/src/components/Results.tsx
+++ bookmark-express-next/src/components/Results.tsx
@@ -26,6 +26,7 @@ const useStyles = makeStyles({
         fontSize: '12px',
         fontStyle: 'italic',
         color: 'gray',
+        wordBreak: 'break-all',
 
         '& .search-hit': {
             backgroundColor: '#3576CB',
