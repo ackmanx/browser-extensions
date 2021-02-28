5bd5d7938d5b82f7bdb89336e175f7df0ac7073f
Eric Majerus <ackmanx@users.noreply.github.com>
2021-02-27 22:02:29 -0600

    adding current page to bookmarks. defaulting to Other Bookmarks until folder selection solution is in place

--- bookmark-express-next/src/components/EditBookmark.tsx
+++ bookmark-express-next/src/components/EditBookmark.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import {Button, Input, makeStyles, TextField} from '@material-ui/core'
-import {browser} from "webextension-polyfill-ts";
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Avatar, Button, InputAdornment, makeStyles, TextField } from '@material-ui/core'
+import { Bookmarks, browser, Tabs } from 'webextension-polyfill-ts'
 
 const useStyles = makeStyles((theme) => ({
     search: {
@@ -17,20 +17,70 @@ const useStyles = makeStyles((theme) => ({
 
 export function EditBookmark() {
     const classes = useStyles()
+    const [activeTab, setActiveTab] = useState<Tabs.Tab>()
+    const [bookmark, setBookmark] = useState<Bookmarks.CreateDetails>()
+
+    useEffect(() => {
+        ;(async () => {
+            const tab = (await browser.tabs.query({ active: true }))[0]
+
+            setActiveTab(tab)
+
+            setBookmark({
+                title: tab.title,
+                url: tab.url,
+            })
+        })()
+    }, [])
+
+    const handleTextChange = (event: ChangeEvent<HTMLInputElement>, key: string) => {
+        setBookmark((prevState) => ({
+            ...prevState,
+            [key]: event.target.value,
+        }))
+    }
 
     const handleAddBookmark = async () => {
-        const tab = (await browser.tabs.query({active: true}))[0]
-        console.log(777, tab.title)
-        console.log(777, tab.url)
-        console.log(777, tab.favIconUrl)
+        if (!bookmark) return
+
+        await browser.bookmarks.create(bookmark)
+
+        window.close()
     }
 
     return (
-        <form noValidate className={classes.form} autoComplete='off'>
-            <h3>Page title here with favicon</h3>
-            <TextField fullWidth className={classes.search} variant='outlined' label='Title' />
-            <TextField fullWidth className={classes.search} variant='outlined' label='URL' />
-            <Button variant="contained" color="primary" onClick={handleAddBookmark}>Add</Button>
-        </form>
+        <>
+            Bookmark Current Page
+            <form noValidate className={classes.form} autoComplete='off'>
+                <TextField
+                    fullWidth
+                    className={classes.search}
+                    variant='outlined'
+                    label='Title'
+                    value={bookmark?.title}
+                    InputLabelProps={{ shrink: true }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position='start'>
+                                <Avatar src={activeTab?.favIconUrl} />
+                            </InputAdornment>
+                        ),
+                    }}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleTextChange(event, 'title')}
+                />
+                <TextField
+                    fullWidth
+                    className={classes.search}
+                    variant='outlined'
+                    label='URL'
+                    value={bookmark?.url}
+                    InputLabelProps={{ shrink: true }}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleTextChange(event, 'url')}
+                />
+                <Button variant='contained' color='primary' onClick={handleAddBookmark}>
+                    Add
+                </Button>
+            </form>
+        </>
     )
 }
