3d7794e1a408030999fae86ae89f7249842f6236
Eric Majerus <ackmanx@users.noreply.github.com>
2021-03-01 18:48:41 -0600

    trying out accordion for folder selection modes

--- bookmark-express-next/src/components/FolderSelection.tsx
+++ bookmark-express-next/src/components/FolderSelection.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core'
+import { Accordion, AccordionDetails, AccordionSummary, makeStyles, Typography } from '@material-ui/core'
 import TreeView from '@material-ui/lab/TreeView'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
@@ -12,12 +12,17 @@ const useStyles = makeStyles((theme) => ({}))
 export function FolderSelection() {
     const classes = useStyles()
     const [allFolders, setAllFolders] = useState<Bookmarks.BookmarkTreeNode>()
+    const [expanded, setExpanded] = React.useState('')
 
     useEffect(() => {
         ;(async () => {
             setAllFolders(await getFolders())
         })()
     }, [])
+
+    const handleChange = (panel: any) => (event: any, isExpanded: any) => {
+        setExpanded(isExpanded ? panel : false)
+    }
 
     const renderFolderTree = (node: Bookmarks.BookmarkTreeNode | undefined) => {
         if (!node || !isFolder(node)) return null
@@ -30,12 +35,39 @@ export function FolderSelection() {
     }
 
     return (
-        <TreeView
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-            defaultExpanded={['0']}
-        >
-            {renderFolderTree(allFolders)}
-        </TreeView>
+        <>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Favorites</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Favorites</Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Recently Used</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Recently Used</Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Browse</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <TreeView
+                        defaultCollapseIcon={<ExpandMoreIcon />}
+                        defaultExpandIcon={<ChevronRightIcon />}
+                        defaultExpanded={['0']}
+                    >
+                        {renderFolderTree(allFolders)}
+                    </TreeView>
+                </AccordionDetails>
+            </Accordion>
+        </>
     )
 }
