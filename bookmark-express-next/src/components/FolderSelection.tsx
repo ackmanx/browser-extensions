9454e4ca969c1c5b72056a0ae9e31a9af8433c44
Eric Majerus <ackmanx@users.noreply.github.com>
2021-03-01 20:17:05 -0600

    using custom accordion

--- bookmark-express-next/src/components/FolderSelection.tsx
+++ bookmark-express-next/src/components/FolderSelection.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Accordion, AccordionDetails, AccordionSummary, makeStyles, Typography } from '@material-ui/core'
+import {
+    Accordion as MuiAccordion,
+    AccordionDetails as MuiAccordionDetails,
+    AccordionSummary as MuiAccordionSummary,
+    Typography,
+    withStyles,
+} from '@material-ui/core'
 import TreeView from '@material-ui/lab/TreeView'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
@@ -7,10 +13,48 @@ import TreeItem from '@material-ui/lab/TreeItem'
 import { Bookmarks } from 'webextension-polyfill-ts'
 import { getFolders, isFolder } from '../utils/misc'
 
-const useStyles = makeStyles((theme) => ({}))
+const Accordion = withStyles({
+    root: {
+        border: '1px solid rgba(0, 0, 0, .125)',
+        boxShadow: 'none',
+        '&:not(:last-child)': {
+            borderBottom: 0,
+        },
+        '&:before': {
+            display: 'none',
+        },
+        '&$expanded': {
+            margin: 'auto',
+        },
+    },
+    expanded: {},
+})(MuiAccordion)
+
+const AccordionSummary = withStyles({
+    root: {
+        backgroundColor: 'rgba(0, 0, 0, .03)',
+        borderBottom: '1px solid rgba(0, 0, 0, .125)',
+        sectionSpacing: -1,
+        minHeight: 56,
+        '&$expanded': {
+            minHeight: 56,
+        },
+    },
+    content: {
+        '&$expanded': {
+            margin: '12px 0',
+        },
+    },
+    expanded: {},
+})(MuiAccordionSummary)
+
+const AccordionDetails = withStyles((theme) => ({
+    root: {
+        padding: theme.spacing(2),
+    },
+}))(MuiAccordionDetails)
 
 export function FolderSelection() {
-    const classes = useStyles()
     const [allFolders, setAllFolders] = useState<Bookmarks.BookmarkTreeNode>()
     const [expanded, setExpanded] = React.useState('')
 
